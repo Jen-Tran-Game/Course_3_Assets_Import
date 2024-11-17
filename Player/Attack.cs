296a306c66b8038c1f421a52c31735d2949f38fe
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-11-17 20:57:22 +0700

    ._.

--- Player/Attack.cs
+++ Player/Attack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    // varible to determine if the damage function can be called 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit: " + other.name);
@@ -12,8 +13,12 @@ public class Attack : MonoBehaviour
 
         if (hit != null)
         {
-            hit.Damage(); 
+            //if can attack
+                hit.Damage(); 
+            // set that variable to false
         }
     }
+
+    //coroutine to reset variable after 0.5f 
 
 }
